bf606d6b42b495f031f80dfda17f4e915c2100c9
liu chen <liuchen0791@163.com>
2022-01-21 20:55:37 +0800

    first

--- MoveZero283.cpp
+++ MoveZero283.cpp
@@ -3,7 +3,24 @@
 //
 
 #include <iostream>
+#include "vector"
 using namespace std;
+class Solution{
+public:
+    void moveZeroes(vector<int> &nums) {
+        int j = 0;
+        for(int i = 0; i < nums.size(); i++)
+            if(nums[i] != 0)
+                nums[j++] = nums[i];
+        while(j < nums.size())
+            nums[j++] = 0;
+    }
+};
+
 int main(){
+    vector<int> nums = {0,1,0,3,12};
+    Solution().moveZeroes(nums);
+    for(int a : nums)
+        cout<<a<<" ";
 
 }
